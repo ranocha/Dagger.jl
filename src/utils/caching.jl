fed1a68f0069f861d4aeb5d0cc79d5d96a2a2274
krynju <gulinski.krystian@gmail.com>
2023-02-12 19:51:00 +0100

    not working prototype

--- src/utils/caching.jl
+++ src/utils/caching.jl
@@ -1,25 +1,42 @@
-function enable_disk_caching(;
-    membound=8 * (1024^3),
-    diskpath=joinpath(MemPool.default_dir(), randstring(6)),
-    diskbound=32 * (1024^3),
-    kind="MRU"
-)
-    if !(kind in ("LRU", "MRU"))
-        @warn "Unknown allocator kind: $kind\nDefaulting to MRU"
-        kind = "MRU"
+function enable_disk_caching!(ram_percentage_limit::Int=30)
+    processes = procs()
+
+    process_info = [ id =>
+        remotecall(id) do
+            return (;
+                total_memory=Sys.total_physical_memory(),
+                hostname=gethostname(),
+            )
+        end
+        for id in processes
+    ]
+
+    machines = Dict()
+    for (id, info) in process_info
+        key = fetch(info)
+        machines[key] = push!(get(machines, key, Int[]), id)
     end
+
+    mem_limits = Dict()
+    for (info, ids) in machines
+        for id in ids
+            mem_limits[id] = info.total_memory * ram_percentage_limit ÷ length(ids)
+        end
+    end
+
+
 
     r = [
         remotecall(id) do
-            MemPool.GLOBAL_DEVICE[] = MemPool.SimpleRecencyAllocator(
-                membound,
-                MemPool.SerializationFileDevice(MemPool.FilesystemResource(), joinpath(diskpath, "process_$id")),
-                diskbound,
-                Symbol(kind)
+            MemPool.setup_global_device!(
+                MemPool.DiskCacheConfig(;
+                    toggle=true,
+                    membound=mem_limits[id]
+                )
             )
             true
         end
-        for id in procs()
+        for id in processes
     ]
     return all(fetch.(r))
 end
